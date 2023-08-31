4033d0065a767faf17c5ec3d896e74c65c07c6eb
Walther96 <walther.choque96@gmail.com>
2023-08-30 23:44:46 -0500

    root driver

--- src/test/java/pe/personal/Test.java
+++ src/test/java/pe/personal/Test.java
@@ -1,6 +1,7 @@
 package pe.personal;
 
 import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.core.Serenity;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -14,16 +15,8 @@ import org.openqa.selenium.chrome.ChromeDriverService;
         glue = "pe.personal")
 public class Test {
 
-    public static String ANSI_GREEN = "\u001B[32m";
-
-    @BeforeClass
-    public static void init() {
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
-    }
-
-    @AfterClass
+    /*@AfterClass
     public static void end() {
-        System.out.println(ANSI_GREEN + ANSI_GREEN);
-    }
-
+        Serenity.getWebdriverManager().getCurrentDriver().quit();
+    }*/
 }
